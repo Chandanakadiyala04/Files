c37be789be016bca1bfa22ed64bde13b2a557bc2
K. Hari Chandana <harichandanakadiyala@gmail.com>
2026-04-27 10:39:56 +0530

    Delete BE ZIP/book_store directory

--- BE ZIP/book_store/src/main/java/com/miniproject/book_store/Repository/BlogsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.miniproject.book_store.Repository;
-
-import com.miniproject.book_store.Entity.Blogs;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogsRepository extends JpaRepository<Blogs,Integer> {
-
-}
